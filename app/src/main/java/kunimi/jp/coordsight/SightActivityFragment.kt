6c7307362f7d0bae1a846a82febe76a9bf134390
KUNIMI Taiyoh <kunimi.taiyoh@gmail.com>
2018-03-23 13:27:30 +0900

    camera view is now available.

--- app/src/main/java/kunimi/jp/coordsight/SightActivityFragment.kt
+++ app/src/main/java/kunimi/jp/coordsight/SightActivityFragment.kt
@@ -1,18 +1,121 @@
 package kunimi.jp.coordsight
 
-import android.support.v4.app.Fragment
+import android.Manifest
+import android.content.Context
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
+import android.hardware.camera2.CaptureRequest
 import android.os.Bundle
+import android.os.Handler
+import android.support.v4.app.ActivityCompat
+import android.support.v4.app.Fragment
 import android.view.LayoutInflater
+import android.view.Surface
+import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
+import java.util.*
 
 /**
  * A placeholder fragment containing a simple view.
  */
 class SightActivityFragment : Fragment() {
+    private var mCameraDevice: CameraDevice? = null
+    private lateinit var mTextureView: TextureView
+    private val mBackgroundHandler = Handler()
+    private var mCaptureSession: CameraCaptureSession? = null
+
+    private val mStateCallback = object : CameraDevice.StateCallback() {
+        override fun onOpened(cameraDevice: CameraDevice) {
+            mCameraDevice = cameraDevice
+            createCameraPreviewSession()
+        }
+
+        override fun onDisconnected(cameraDevice: CameraDevice) {
+            cameraDevice.close()
+            mCameraDevice = null
+        }
+
+        override fun onError(cameraDevice: CameraDevice, error: Int) {
+            cameraDevice.close()
+            mCameraDevice = null
+        }
+
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_sight, container, false)
+        val root = inflater.inflate(R.layout.fragment_sight, container, false)
+        mTextureView = root.findViewById(R.id.texture) as TextureView
+        mTextureView.surfaceTextureListener = object : TextureView.SurfaceTextureListener {
+            override fun onSurfaceTextureAvailable(surface: SurfaceTexture, width: Int, height: Int) {
+                openCamera()
+            }
+
+            override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture, width: Int, height: Int) {
+            }
+
+            override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
+                return true
+            }
+
+            override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {
+            }
+        }
+
+        return root
+    }
+
+    private fun openCamera() {
+        try {
+            val manager = activity.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+            val selectedCameraId = manager.cameraIdList
+                    .firstOrNull { manager.getCameraCharacteristics(it).get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_BACK }
+            if (selectedCameraId != null) {
+                ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.CAMERA), 1)
+                manager.openCamera(selectedCameraId, mStateCallback, mBackgroundHandler)
+            }
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun createCameraPreviewSession() {
+        val texture = mTextureView.surfaceTexture
+        texture.setDefaultBufferSize(mTextureView.width, mTextureView.height)
+        val surface = Surface(texture)
+
+        try {
+            val builder = mCameraDevice!!.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+            builder.addTarget(surface)
+            val previewRequest = builder.build()
+
+            mCameraDevice!!.createCaptureSession(Arrays.asList(surface), object : CameraCaptureSession.StateCallback() {
+                override fun onConfigured(session: CameraCaptureSession) {
+                    if (null == mCameraDevice) {
+                        return
+                    }
+
+                    mCaptureSession = session
+
+                    try {
+                        session.setRepeatingRequest(previewRequest, null, null)
+                    } catch (e: CameraAccessException) {
+                        e.printStackTrace()
+                    }
+                }
+
+                override fun onConfigureFailed(session: CameraCaptureSession) {
+                }
+            }, null)
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
     }
 }
